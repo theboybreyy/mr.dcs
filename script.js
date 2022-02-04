fae8a1babf33d1432bc26208385b0415adaadeca
Lex Benjamin <86781764+theboybreyy@users.noreply.github.com>
2022-02-04 17:37:32 +0000

    Update script.js

--- script.js
+++ script.js
@@ -9,13 +9,3 @@ function openForm() {
 function levels(){
   document.getElementById("sec_nav").style.display ="block";
 }
-
-function display(){
-  if(document.getElementsByClassName("myForm").style.display ="block"){
-      document.getElementsByClassName("main").style.display ="none";
-    }
-    else{
-      document.getElementsByClassName("main").style.display ="block";
-      
-  }
-}
