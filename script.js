a1781375f8af053c8a1dacafc222b0284e4a1801
Lex Benjamin <86781764+theboybreyy@users.noreply.github.com>
2022-02-11 14:35:05 +0000

    Made some few changes

--- script.js
+++ script.js
@@ -5,3 +5,12 @@ function openForm() {
   function closeForm() {
     document.getElementById("myForm").style.display = "none";
   }
+
+
+  /*Added Some GSAP Animation*/
+  const el = document.querySelector("#module");
+
+el.addEventListener("onclick", (e) => {
+  el.style.backgroundPositionX = -e.offsetX + "px";
+  el.style.backgroundPositionY = -e.offsetY + "px";
+});
