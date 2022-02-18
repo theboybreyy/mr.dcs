9a3a5e8ad7efbb69e8d9bf816ae5b858e9ce1095
Lex Benjamin <86781764+theboybreyy@users.noreply.github.com>
2022-02-18 00:41:50 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 /*This is a very long line commit history to scare people k3k3
-Dont rush life is not a race God first and all w*/
+Dont rush life is not a race God first and all */
 
 function openForm() {
     document.getElementById("myForm").style.display = "block";
