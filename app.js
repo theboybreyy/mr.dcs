8172863c106a58b69c94b1045c190aadeeb99753
Lex Benjamin <86781764+theboybreyy@users.noreply.github.com>
2022-02-27 13:32:27 +0000

    Updates underway...

--- app.js
+++ app.js
@@ -4,3 +4,30 @@ const preloader = document.querySelector(".preloader");
 window.addEventListener("load", () => {
     preloader.style.display = "none";
 })
+
+//Navbar Effect
+const hambuger = document.querySelector(".hamburger");
+hambuger.addEventListener("click", function () {
+    this.classList.toggle('active');
+})
+
+//Adding The About Dropdown
+function openAbout() {
+    document.querySelector('.drop-downs').style.display = "block";
+}
+function closeAbout() {
+    document.querySelector('.drop-downs').style.display = "none";
+}
+
+//Join Groups Dropdown
+function openGroup() {
+    document.querySelector('.pop-down').style.display = "block";
+}
+function closeGroup() {
+    document.querySelector('.pop-down').style.display = "none";
+}
+
+var about = document.getElementsByClassName("drop-downs");
+var group = document.getElementsByClassName("pop-down");
+
+PictureInPictureWindow.style.overflow = "none";
