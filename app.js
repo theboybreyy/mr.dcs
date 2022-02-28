be5181df7b2387ccd805e7ccba1ae91d30637d99
Lex Benjamin <86781764+theboybreyy@users.noreply.github.com>
2022-02-28 14:42:34 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-/This s single lie comment that i will be deleting anytime so
+/This s single lie comment that i will be deleting anytime 
 //Preloader effect
 
 const preloader = document.querySelector(".preloader");
