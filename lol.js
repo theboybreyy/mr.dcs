9fa52c0f00241c5e07dd0b129f38acf5a3bac718
Lex Benjamin <86781764+theboybreyy@users.noreply.github.com>
2022-03-07 18:08:20 +0000

    Update lol.js

--- lol.js
+++ lol.js
@@ -1,4 +1,4 @@
-//This is a single line comment that i wi
+//This is a single line comment that 
 const toggleDarkMode = document.querySelector(".toggle-darkmode");
 const toggleText = document.querySelector(".toggle-text");
 
