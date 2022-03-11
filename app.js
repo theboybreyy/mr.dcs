c77043c204defc914c00dd0b83555499cebae2d2
Lex Benjamin <abessilfie@st.ug.edu.gh>
2022-03-11 14:03:43 +0000

    Lille

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 
-//This is a single line comment that I am adding i will just remove t
+//This is a single line comment that I am adding i will just remov
 //Preloader effect
 
 const preloader = document.querySelector(".preloader");
