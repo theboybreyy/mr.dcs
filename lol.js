caa8c6d17bf0dbdf57de10af6c1f0a505a9a256d
Lex Benjamin <86781764+theboybreyy@users.noreply.github.com>
2022-03-25 23:29:42 +0800

    Update lol.js

--- lol.js
+++ lol.js
@@ -1,4 +1,15 @@
-//This is hust a ingle line com
+const sign_in_btn = document.querySelector("#sign-in-btn");
+const sign_up_btn = document.querySelector("#sign-up-btn");
+const container = document.querySelector(".container");
+
+sign_up_btn.addEventListener('click', () => {
+    container.classList.add("sign-up-mode");
+});
+sign_in_btn.addEventListener('click', () => {
+    container.classList.remove("sign-up-mode");
+});
+
+
 
 
 const toggleDarkMode = document.querySelector(".toggle-darkmode");
