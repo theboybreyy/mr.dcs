e6757bdeb2539bd905a11cb3f31fa4c7d8683bcf
Lex Benjamin <86781764+theboybreyy@users.noreply.github.com>
2022-03-25 23:30:09 +0800

    Update lol.js

--- lol.js
+++ lol.js
@@ -23,6 +23,61 @@ const enableDarkMode = () => {
   toggleText.textContent = "Light";
   localStorage.setItem("darkMode", "enabled");
 };
+const toggleDarkMode = document.querySelector(".toggle-darkmode");
+const toggleText = document.querySelector(".toggle-text");
+
+let darkMode = localStorage.getItem("darkMode");
+
+// Set Dark Mode
+const enableDarkMode = () => {
+  document.body.classList.add("darkmode");
+  toggleText.textContent = "Light";
+  localStorage.setItem("darkMode", "enabled");
+};
+const toggleDarkMode = document.querySelector(".toggle-darkmode");
+const toggleText = document.querySelector(".toggle-text");
+
+let darkMode = localStorage.getItem("darkMode");
+
+// Set Dark Mode
+const enableDarkMode = () => {
+  document.body.classList.add("darkmode");
+  toggleText.textContent = "Light";
+  localStorage.setItem("darkMode", "enabled");
+};
+const toggleDarkMode = document.querySelector(".toggle-darkmode");
+const toggleText = document.querySelector(".toggle-text");
+
+let darkMode = localStorage.getItem("darkMode");
+
+// Set Dark Mode
+const enableDarkMode = () => {
+  document.body.classList.add("darkmode");
+  toggleText.textContent = "Light";
+  localStorage.setItem("darkMode", "enabled");
+};
+const toggleDarkMode = document.querySelector(".toggle-darkmode");
+const toggleText = document.querySelector(".toggle-text");
+
+let darkMode = localStorage.getItem("darkMode");
+
+// Set Dark Mode
+const enableDarkMode = () => {
+  document.body.classList.add("darkmode");
+  toggleText.textContent = "Light";
+  localStorage.setItem("darkMode", "enabled");
+};
+const toggleDarkMode = document.querySelector(".toggle-darkmode");
+const toggleText = document.querySelector(".toggle-text");
+
+let darkMode = localStorage.getItem("darkMode");
+
+// Set Dark Mode
+const enableDarkMode = () => {
+  document.body.classList.add("darkmode");
+  toggleText.textContent = "Light";
+  localStorage.setItem("darkMode", "enabled");
+};
 
 // Disable Dark Mode
 const disableDarkMode = () => {
