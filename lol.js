83a630a040598850419e97ae6042e6ac2cac37e8
Lex Benjamin <86781764+theboybreyy@users.noreply.github.com>
2022-03-27 22:25:08 +0000

    Update lol.js

--- lol.js
+++ lol.js
@@ -10,6 +10,59 @@ sign_in_btn.addEventListener('click', () => {
 });
 
 
+const toggleDarkMode = document.querySelector(".toggle-darkmode");
+const toggleText = document.querySelector(".toggle-text");
+
+let darkMode = localStorage.getItem("darkMode");
+
+// Set Dark Mode
+const enableDarkMode = () => {
+  document.body.classList.add("darkmode");
+  toggleText.textContent = "Light";
+  localStorage.setItem("darkMode", "enabled");
+};
+const toggleDarkMode = document.querySelector(".toggle-darkmode");
+const toggleText = document.querySelector(".toggle-text");
+
+let darkMode = localStorage.getItem("darkMode");
+
+// Set Dark Mode
+const enableDarkMode = () => {
+  document.body.classList.add("darkmode");
+  toggleText.textContent = "Light";
+  localStorage.setItem("darkMode", "enabled");
+};
+const toggleDarkMode = document.querySelector(".toggle-darkmode");
+const toggleText = document.querySelector(".toggle-text");
+
+let darkMode = localStorage.getItem("darkMode");
+const toggleDarkMode = document.querySelector(".toggle-darkmode");
+const toggleText = document.querySelector(".toggle-text");
+
+let darkMode = localStorage.getItem("darkMode");
+
+// Set Dark Mode
+const enableDarkMode = () => {
+  document.body.classList.add("darkmode");
+  toggleText.textContent = "Light";
+  localStorage.setItem("darkMode", "enabled");
+};
+const toggleDarkMode = document.querySelector(".toggle-darkmode");
+const toggleText = document.querySelector(".toggle-text");
+
+let darkMode = localStorage.getItem("darkMode");
+
+// Set Dark Mode
+const enableDarkMode = () => {
+  document.body.classList.add("darkmode");
+  toggleText.textContent = "Light";
+  localStorage.setItem("darkMode", "enabled");
+};
+const toggleDarkMode = document.querySelector(".toggle-darkmode");
+const toggleText = document.querySelector(".toggle-text");
+
+let darkMode = localStorage.getItem("darkMode");
+
 
 
 const toggleDarkMode = document.querySelector(".toggle-darkmode");
