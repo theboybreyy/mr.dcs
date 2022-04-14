1845d67c7e5b97eb4642afb18c60773cff065b48
Lex Benjamin <86781764+theboybreyy@users.noreply.github.com>
2022-04-14 19:17:41 +0000

    Update DCS.java

--- DCS.java
+++ DCS.java
@@ -2,7 +2,7 @@
     /
     // The total amount of money collected by this machine.
     private int total;
- The price is given to customers
+ The price is gcustomers
     
     /**
      * Create a machine that issues tickets of the given price.public class TicketMachine
